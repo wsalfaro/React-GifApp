f683b0ace6fed50a53ada5f1feaa82862745ba62
wesley <wsarcenoa1@miumg.edu.gt>
2020-08-31 18:29:44 -0600

    Docs agregado

---
+++ docs/precache-manifest.24d4b88d1ce1921dc46e582b1c6bec9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "594609e582efd3e196b94977a1803a93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87f84bc0890f4ba75942",
+    "url": "/static/css/main.bb58a887.chunk.css"
+  },
+  {
+    "revision": "c891655c70870ca4f1e5",
+    "url": "/static/js/2.00eec8fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.00eec8fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87f84bc0890f4ba75942",
+    "url": "/static/js/main.aedce366.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
